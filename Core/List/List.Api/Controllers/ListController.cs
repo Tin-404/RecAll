160503c797c4f7ae07e2f05766290409969663f8
Tin-404 <846013863@qq.com>
2023-04-06 14:31:22 +0800

    实现IRepository; 创建应用层

---
+++ Core/List/List.Api/Controllers/ListController.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using RecAll.Core.List.Api.Application.Commands;
+using RecAll.Core.List.Api.Infrastructure.Services;
+using RecAll.Infrastructure.Api;
+
+namespace RecAll.Core.List.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ListController {
+    private readonly IMediator _mediator;
+    private readonly ILogger<ListController> _logger;
+    private readonly IIdentityService _identityService;
+    
+    [Route("create")]
+    [HttpPost]
+    public async Task<ActionResult<ServiceResultViewModel>> CreateAsync(
+        [FromBody] CreateListCommand command) {
+        _logger.LogInformation(
+            "----- Sending command: {CommandName} - UserIdentityGuid: {userIdentityGuid} ({@Command})",
+            command.GetType().Name, _identityService.GetUserIdentityGuid(),
+            command);
+        
+        return (await _mediator.Send(command)).ToServiceResultViewModel();
+    }
+}
